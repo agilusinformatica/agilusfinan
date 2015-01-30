2220cf6f6570753219439468083c638fdbd52f6a
agilusinformatica <claudio@agilus.com.br>
2015-01-30 18:03:26 -0200

    add context.cs

---
+++ AgilusFinan.Infra/Context/Context.cs
@@ -0,0 +1,27 @@
+﻿using AgilusFinan.Domain.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AgilusFinan.Infra.Context
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base("dbAgilusFinan")
+        {
+
+        }
+
+        public DbSet<Banco> Bancos { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<TipoTelefone> TiposTelefone { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+    }
+}
