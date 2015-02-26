b84553b427aafb8f48e298d403b02a294fc4176e
Rafael <rafaell.rochaa@gmail.com>
2015-02-26 11:57:07 -0300

    arq FormaPagamento.cs

---
+++ AgilusFinan.Domain/Entities/FormaPagamento.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgilusFinan.Domain.Entities
+{
+    public enum FormaPagamento { Boleto, Dinheiro, CartaoDebito, CartaoCredito, Transferencia }
+}
