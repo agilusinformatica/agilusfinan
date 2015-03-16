577fdafa2ccfc9c7aae382642ff7602d2bc82fee
Carlos Saraiva <carlos.saraiva@msn.com>
2015-03-16 15:22:52 -0300

    Nome campo 'CategoriaPai' para 'CategoriaPaiId'

--- AgilusFinan.Infra/Migrations/201503151552218_NullCategoriaPaiId.cs
+++ AgilusFinan.Infra/Migrations/201503151552218_NullCategoriaPaiId.cs
@@ -10,13 +10,13 @@ namespace AgilusFinan.Infra.Migrations
             DropIndex("dbo.Categoria", new[] { "CategoriaPaiId" });
             AlterColumn("dbo.Categoria", "CategoriaPaiId", c => c.Int());
             CreateIndex("dbo.Categoria", "CategoriaPaiId");
-            Sql("update dbo.Categoria set CategoriaPaiId = null where Id = CategoriaPai");
+            Sql("update dbo.Categoria set CategoriaPaiId = null where Id = CategoriaPaiId");
         }
         
         public override void Down()
         {
             DropIndex("dbo.Categoria", new[] { "CategoriaPaiId" });
-            Sql("update dbo.Categoria set CategoriaPaiId = Id where CategoriaPai is null");
+            Sql("update dbo.Categoria set CategoriaPaiId = Id where CategoriaPaiId is null");
             AlterColumn("dbo.Categoria", "CategoriaPaiId", c => c.Int(nullable: false));
             CreateIndex("dbo.Categoria", "CategoriaPaiId");
         }
