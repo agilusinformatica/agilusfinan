863c8bf7c978a26b045d0fbca406f5a1cf2226d6
Rafael <rafaell.rochaa@gmail.com>
2015-06-12 16:18:56 -0300

    Criação controller/view  models Recebimento e Conta

---
+++ AgilusFinan.Web/Controllers/ContaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgilusFinan.Web.Bases;
+using AgilusFinan.Infra.Services;
+using AgilusFinan.Domain.Entities;
+
+namespace AgilusFinan.Web.Controllers
+{
+    public class ContaController : ControllerPadrao<Conta,RepositorioConta>
+    {
+        protected override void PreInclusao()
+        {
+            base.PreInclusao();
+            ViewBag.BancoBoletoId = new SelectList(new RepositorioBanco().Listar(),"Id","Nome");
+        }
+
+    }
+}
---
+++ AgilusFinan.Web/Controllers/RecebimentoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgilusFinan.Web.Bases;
+using AgilusFinan.Domain.Entities;
+using AgilusFinan.Infra.Services;
+using System.Collections;
+
+namespace AgilusFinan.Web.Controllers
+{
+    public class RecebimentoController : ControllerPadrao<Recebimento, RepositorioRecebimento>
+    {
+        protected override void PreInclusao()
+        {
+            base.PreInclusao();
+            GerarListaCampos();
+        }
+
+        private void GerarListaCampos()
+        {
+            ViewBag.CategoriaId = new SelectList(new RepositorioCategoria().Listar(c => c.Direcao == DirecaoCategoria.Recebimento).ToList(), "Id", "Nome");
+            ViewBag.PessoaId = new SelectList(new RepositorioPessoa().Listar(), "Id", "Nome");
+            ViewBag.CentroCustoId = new SelectList(new RepositorioCentroCusto().Listar(), "Id", "Nome");
+            ViewBag.ContaId = new SelectList(new RepositorioConta().Listar(), "Id", "Nome");
+        }
+
+        protected override void PreAlteracao()
+        {
+            base.PreAlteracao();
+            GerarListaCampos();
+
+        }
+
+    }
+}
