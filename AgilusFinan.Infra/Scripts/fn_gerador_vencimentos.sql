421f4284234ec8d3592b14d9c56733c1a502d3d1
agilusinformatica <claudio@agilus.com.br>
2015-07-20 16:26:11 -0300

    Inclusão das procedures de geração de títulos virtuais (by Rafael)

---
+++ AgilusFinan.Infra/Scripts/fn_gerador_vencimentos.sql
@@ -0,0 +1,56 @@
+create function fn_gerador_vencimentos(@data_inicial datetime, @data_final datetime, @dia_vencimento tinyint, @qtde_parcelas int, @data_primeiro_vencimento datetime, @tipo_recorrencia tinyint)
+RETURNS @vencimentos table(data_vencimento datetime)  AS
+BEGIN 
+	declare 
+	   @cont int = 0,
+      @data_final_analise datetime = dateadd(week, +1, @data_final)
+
+   declare 
+      @data_base datetime = @data_primeiro_vencimento
+
+	WHILE @data_base <= @data_final_analise
+	BEGIN
+      set @data_base = 
+         case @tipo_recorrencia 
+            when 0 then dateadd(week, @cont, @data_primeiro_vencimento)
+            when 1 then dateadd(week, @cont*2, @data_primeiro_vencimento)
+            when 2 then dateadd(MONTH, @cont, @data_primeiro_vencimento)
+            when 3 then dateadd(MONTH, @cont*2, @data_primeiro_vencimento)
+            when 4 then dateadd(MONTH, @cont*3, @data_primeiro_vencimento)
+            when 5 then dateadd(MONTH, @cont*6, @data_primeiro_vencimento)
+            when 6 then dateadd(year, @cont, @data_primeiro_vencimento)
+         end
+      
+		
+      if @dia_vencimento is not null and @tipo_recorrencia >= 2
+			set @data_base = @data_base-day(@data_base)+@dia_vencimento
+      if @dia_vencimento is not null and @tipo_recorrencia in (0,1)
+         while datepart(dw, @data_base) != @dia_vencimento
+			   set @data_base = @data_base + 1
+
+			
+		WHILE /*dbo.fn_feriado(@data_base) = 1 or */ DATEPART(DW,@data_base) in (7, 1)
+		BEGIN
+			set @data_base = @data_base-1
+		END
+
+      if (@data_base between @data_inicial and @data_final and @data_base >= @data_primeiro_vencimento) 
+			insert into @vencimentos(data_vencimento) values(@data_base)
+
+		set @cont = @cont + 1
+
+      if @cont > @qtde_parcelas-1
+         return
+	END
+   return
+END
+
+/*
+select * from fn_gerador_vencimentos('2015-07-01', '2015-07-31', 2, null, '2015-07-15', 0)
+select * from fn_gerador_vencimentos('2015-07-01', '2015-07-31', 2, null, '2015-07-01', 1)
+select * from fn_gerador_vencimentos('2015-07-01', '2015-07-31', 2, null, '2015-07-15', 2)
+select * from fn_gerador_vencimentos('2015-01-01', '2015-12-31', 2, null, '2015-01-01', 3)
+select * from fn_gerador_vencimentos('2015-01-01', '2015-12-31', 2, null, '2015-01-01', 4)
+select * from fn_gerador_vencimentos('2015-01-01', '2015-12-31', 2, null, '2015-01-01', 5)
+select * from fn_gerador_vencimentos('2015-01-01', '2016-12-31', 2, null, '2015-01-01', 6)
+*/
---
+++ AgilusFinan.Infra/Scripts/pr_cria_titulo_virtual.sql
@@ -0,0 +1,48 @@
+create procedure pr_cria_titulo_virtual(@id_empresa int, @data_inicial_analise smalldatetime, @data_final_analise smalldatetime) as
+Begin
+
+	declare @titulo_virtual table(
+	TituloRecorrenteId int,
+	nome varchar(100),
+	DataVencimento datetime, 
+	Valor money, 
+	CategoriaId int, 
+	PessoaId int, 
+	CentroCustoId int,
+	EmpresaId int)
+
+	declare @id int,
+	@nome varchar(100),
+	@dia_vencimento tinyint,
+	@valor money,
+	@recorrencia tinyint,
+	@qtde_parcelas int,
+	@categoria_id int,
+	@pessoa_id int,
+	@centro_custo_id int,
+	@data_cadastro datetime
+
+	declare cur cursor for
+	select id, nome, diaVencimento, valor, recorrencia, QtdeParcelas, CategoriaId, PessoaId, CentroCustoId, DataCadastro
+	from TituloRecorrente
+	where EmpresaId = @id_empresa
+	and ativo = 1
+
+	open cur
+	Fetch cur into @id, @nome, @dia_vencimento, @valor, @recorrencia, @qtde_parcelas, @categoria_id, @pessoa_id, @centro_custo_id, @data_cadastro
+	While @@FETCH_STATUS = 0
+	begin
+		insert into @titulo_virtual
+		select @id, @nome, vencimento.*, @valor, @categoria_id, @pessoa_id, @centro_custo_id, @id_empresa
+		from dbo.fn_gerador_vencimentos_recorrentes(@data_inicial_analise, @data_final_analise, @dia_vencimento, @recorrencia, @qtde_parcelas, @data_cadastro) as vencimento
+
+		Fetch cur into @id, @nome, @dia_vencimento, @valor, @recorrencia, @qtde_parcelas, @categoria_id, @pessoa_id, @centro_custo_id, @data_cadastro
+	end
+	close cur
+	deallocate cur
+
+	select *
+	from @titulo_virtual
+end
+
+
