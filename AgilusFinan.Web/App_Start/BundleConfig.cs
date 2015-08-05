f81157b92469f5be39491e18d8429efc0c5fefff
agilusinformatica <claudio@agilus.com.br>
2015-08-05 14:03:31 -0300

    Resolução da validação de decimais

--- AgilusFinan.Web/App_Start/BundleConfig.cs
+++ AgilusFinan.Web/App_Start/BundleConfig.cs
@@ -8,12 +8,15 @@ namespace AgilusFinan.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.mask.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/methods_pt.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
