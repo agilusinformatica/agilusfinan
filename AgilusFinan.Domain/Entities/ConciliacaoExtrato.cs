0212dbe7ec4efd7f1949beeadf584d8b4f3b9e83
rafaellrochaa <rafaell.rochaa@gmail.com>
2015-12-04 19:56:20 -0200

    Criação da Entidade de conciliação de extratos bancários e Classe Parser, com o método de leitura de um documento OFX.

---
+++ AgilusFinan.Domain/Entities/ConciliacaoExtrato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgilusFinan.Domain.Entities
+{
+    public class ConciliacaoExtrato
+    {
+        //Objeto que encapsula as informações contidas em <STMTTRN> do arquivo OFX
+        public int? Id { get; set; }
+        public tipoLancamento TipoLancamento { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public double Valor { get; set; }
+        public string Descricao { get; set; }
+    }
+    public enum tipoLancamento { Debito, Credito }
+}
