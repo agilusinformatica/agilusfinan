2c14de80e3ef586a1090a0dd6bba4bd5269969fe
agilusinformatica <claudio@agilus.com.br>
2015-12-27 06:41:00 -0200

    Implementação do PainelPeriodo em Transferência

--- AgilusFinan.Web/Controllers/TransferenciaController.cs
+++ AgilusFinan.Web/Controllers/TransferenciaController.cs
@@ -4,6 +4,7 @@ using AgilusFinan.Web.Bases;
 using AgilusFinan.Web.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,5 +26,27 @@ namespace AgilusFinan.Web.Controllers
             ViewBag.ContaOrigemId = new SelectList(new RepositorioConta().Listar(), "Id", "Nome");
             ViewBag.ContaDestinoId = new SelectList(new RepositorioConta().Listar(), "Id", "Nome");
         }
+
+        protected override IEnumerable<Transferencia> Dados()
+        {
+            return new List<Transferencia>();
+        }
+
+        public ActionResult IndexData(string dataInicial, string dataFinal)
+        {
+            DateTime dI, dF;
+            if (String.IsNullOrEmpty(dataInicial) && String.IsNullOrEmpty(dataFinal))
+            {
+                dI = Util.PrimeiroDiaMes(DateTime.Today);
+                dF = Util.UltimoDiaMes(DateTime.Today);
+            }
+            else
+            {
+                dI = DateTime.ParseExact(dataInicial, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                dF = DateTime.ParseExact(dataFinal, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            }
+            return PartialView("~/Views/Transferencia/IndexData.cshtml", repo.Listar(t => t.Data >= dI && t.Data <= dF));
+        }
+
     }
 }
