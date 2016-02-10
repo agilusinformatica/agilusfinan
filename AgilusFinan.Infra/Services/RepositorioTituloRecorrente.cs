187e4809a94c8f015e325af14ad2e7dab30d8fed
Henrique <henriquevc93@gmail.com>
2016-02-10 18:02:35 -0200

    Mudança na data de vencimento do Boleto Gerado quando o dia de vencimento do titulo recorrente for alterado

--- AgilusFinan.Infra/Services/RepositorioTituloRecorrente.cs
+++ AgilusFinan.Infra/Services/RepositorioTituloRecorrente.cs
@@ -9,5 +9,15 @@ namespace AgilusFinan.Infra.Services
 {
     public class RepositorioTituloRecorrente : RepositorioPadrao<TituloRecorrente>, IRepositorioTituloRecorrente
     {
+        public override void PreAlteracao(TituloRecorrente obj)
+        {
+            base.PreAlteracao(obj);
+
+            var repositorio = new RepositorioBoletoGerado();
+            var boletogerado = repositorio.Listar(b => b.TituloRecorrenteId == obj.Id).FirstOrDefault();
+            boletogerado.DataVencimento = new DateTime(boletogerado.DataVencimento.Value.Year, boletogerado.DataVencimento.Value.Month, obj.DiaVencimento);
+            repositorio.Alterar(boletogerado);
+        }
+        
     }
 }
