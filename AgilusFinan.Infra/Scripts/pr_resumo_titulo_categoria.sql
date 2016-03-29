1bce9e44bb1e6537e0b632885752d43b7de6ab14
Henrique <henriquevc93@gmail.com>
2016-03-29 18:05:20 -0300

    resumo de titulos calculando também os titulos virtuais

--- AgilusFinan.Infra/Scripts/pr_resumo_titulo_categoria.sql
+++ AgilusFinan.Infra/Scripts/pr_resumo_titulo_categoria.sql
@@ -8,18 +8,46 @@ GO
 
 create procedure pr_resumo_titulo_categoria(@id_empresa int, @data_inicial smalldatetime, @data_final smalldatetime)
 /*----------------------------------------------------------------------------------------------------------------------
-NOME: pr_cria_titulo_virtual
+NOME: pr_resumo_titulo_categoria
 OBJETIVO: Consulta de valores dos títulos por categoria
 DATA: 07/08/2015
-EXEMPLO: exec pr_resumo_titulo_categoria 1,'20150101','20151231'
+EXEMPLO: exec pr_resumo_titulo_categoria 1, '2016-03-01', '2016-03-31'
 ----------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------*/
 as 
 Begin
-	 select Id as CategoriaId, Nome, CategoriaPaiId, Cor, dbo.fn_soma_categoria(id, @data_inicial, @data_final) as Valor
-	 from Categoria 
-	 where EmpresaId = @id_empresa
-	 order by Direcao, CategoriaPaiId
+	create table #temp (Id int not null primary key, Nome varchar(100), CategoriaPaiId int, Cor int, Valor decimal(18,2))
+	
+	insert into #temp 
+	select Id as CategoriaId, Nome, CategoriaPaiId, Cor, 0.0
+	from Categoria 
+	where EmpresaId = @id_empresa
+	order by Direcao, CategoriaPaiId
+
+	declare @valor decimal(18,2)
+	declare @Id int
+	declare cur cursor local for
+	select Id
+	from #temp
+
+	open cur
+	fetch cur into @Id
+
+	While @@FETCH_STATUS = 0
+	begin
+		exec pr_soma_categoria @Id, @data_inicial, @data_final, @valor output
+		update #temp
+		set valor = isnull(@valor, 0)
+		where id = @Id
+
+		fetch cur into @id
+	end
+
+	close cur
+	deallocate cur
+
+	select Id as CategoriaId, Nome, CategoriaPaiId, Cor, Valor 
+	from #temp
 end
 GO
 
@@ -28,3 +56,4 @@ begin
    print '<< CREATE pr_resumo_titulo_categoria >>'
 end
 GO
+
