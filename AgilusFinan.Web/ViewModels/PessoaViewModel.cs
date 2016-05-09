1f9bec4197de202ad7dd3e2c2787072ca1abde79
Henrique <henriquevc93@gmail.com>
2016-05-09 18:06:42 -0300

    adicionado o ibox em todas as Index (finalização)

--- AgilusFinan.Web/ViewModels/PessoaViewModel.cs
+++ AgilusFinan.Web/ViewModels/PessoaViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using AgilusFinan.Domain.Entities;
 using AgilusFinan.Infra.Services;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -14,19 +15,20 @@ namespace AgilusFinan.Web.ViewModels
         [Required(ErrorMessage = "Informe o CPF/CNPJ")]
         [Display(Name = "CPF/CNPJ")]
         public string Cpf { get; set; }
-        [Required]
         [Display(Name = "RG")]
         public string Rg { get; set; }
         [Display(Name = "Data de Nascimento")]
         public DateTime? DataNascimento { get; set; }
         public Endereco Endereco { get; set; }
         [Display(Name = "E-mail de Contato")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Digite um endereço de e-mail válido")]
         public string EmailContato { get; set; }
         [Display(Name = "E-mail Financeiro")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Digite um endereço de e-mail válido")]
         public string EmailFinanceiro { get; set; }
         public ContaBancaria ContaBancaria { get; set; }
-        [Required]
         [Display(Name = "Tipo da Pessoa")]
+        [Required]
         public List<int> TiposPorPessoa { get; set; }
         public List<TelefonePessoaViewModel> Telefones { get; set; }
 
@@ -43,11 +45,21 @@ namespace AgilusFinan.Web.ViewModels
     public class TelefonePessoaViewModel
     {
         public int Id { get; set; }
-
         public string Numero { get; set; }
-
         public string Ddd { get; set; }
-
         public int TipoTelefoneId { get; set; }
+    }
+
+    public class EnsureOneElementAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var list = value as IList;
+            if (list != null)
+            {
+                return list.Count > 0;
+            }
+            return false;
+        }
     }
 }
