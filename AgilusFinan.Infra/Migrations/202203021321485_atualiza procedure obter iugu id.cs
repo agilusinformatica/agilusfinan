50456ea39245b613cd97b9082a714299cee01949
henriquevc <henriquevc93@gmail.com>
2022-03-03 08:55:35 -0400

    trazendo mais campos na procedure de obter iugu id

---
+++ AgilusFinan.Infra/Migrations/202203021321485_atualiza procedure obter iugu id.cs
@@ -0,0 +1,16 @@
+﻿namespace AgilusFinan.Infra.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class atualizaprocedureobteriuguid : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
